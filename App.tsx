8bd1883a56733a8585c922883a6f843ca20632e8
yavuz <yavuz.akpinar.68@gmail.com>
2021-09-30 02:45:41 +0300

    app güncellendi

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import List from './components/CardComponents/list';
 import patikastore from "./Components/CardComponents/patikastore.json"
 import styles from './components/CardComponents/styles';
@@ -8,15 +8,22 @@ import Ara from './components/CardComponents/Ara';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <SafeAreaView style={styles.textInput_view_container}>
-        <Text style={styles.text_patika_container}>Patikastore</Text>
-        {<Ara></Ara>}
-      </SafeAreaView>
+    <View style={styles1.view_container}>
+      <View style={styles.textInput_view_container}>
+        <Text style={styles.text_patika_container}>Patikastore</Text>{<Ara></Ara>}
+      </View>
       <List></List>
       
-    </SafeAreaView>
+    </View>
   );
 }
 
-
+const styles1 = StyleSheet.create({
+  text_container:{
+    
+    
+  },
+  view_container:{
+    flex: 1
+  }
+})
