410706719a30754c20bf2d68a4dbd737424bf0a6
yavuz <yavuz.akpinar.68@gmail.com>
2021-11-14 19:09:26 +0300

    last changes

--- App.tsx
+++ App.tsx
@@ -1,18 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import List from './components/CardComponents/list';
-import patikastore from "./Components/CardComponents/patikastore.json"
+import patikastore from "./components/CardComponents/patikastore.json"
 import styles from './components/CardComponents/styles';
 import Ara from './components/CardComponents/Ara';
+import Card from './components/CardComponents/Card';
 
 export default function App() {
+  const [keyword, setKeyword] = useState('')
+  let source = patikastore.filter(e => e.title.includes(keyword)) ?? "";
   return (
     <View style={styles1.view_container}>
       <View style={styles.textInput_view_container}>
-        <Text style={styles.text_patika_container}>Patikastore</Text>{<Ara></Ara>}
+        <Text style={styles.text_patika_container}>Patikastore</Text>
+        <Ara placeholder="ara beni yala benis" value={keyword} onChangeText={(e: any) => setKeyword(e)} />
       </View>
-      <List></List>
+      <FlatList
+      horizontal={false}
+      numColumns={2}
+      data={source}
+      renderItem={({item}) => <Card
+        title={item.title}
+        fiyat={item.price}
+        image={item.imgURL}
+        instock={item.inStock}
+         />}
+      />
       
     </View>
   );
--- components/CardComponents/Ara.tsx
+++ components/CardComponents/Ara.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, TextInput, View } from "react-native";
 import styles from "./styles";
-
-const Ara = () => {
-  const [text, onChangeText] = React.useState("Ara...");
-  const [number, onChangeNumber] = React.useState(null);
+interface AraProps {
+  placeholder: string
+  value: string
+  onChangeText: () => void
+}
+const Ara = ({placeholder, onChangeText, value}: AraProps) => {
   return(
     <View>
       <TextInput
         style={styles.textInput_container}
-        value={text}
+        placeholder={placeholder}
+        value={value}
         onChangeText={onChangeText}>
       </TextInput>
     </View>
